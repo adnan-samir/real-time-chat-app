d5c2f398acf458adc92dff3d8e37839057615b41
Adnan Samir <116300210+adnan-samir@users.noreply.github.com>
2023-10-21 11:08:48 +0530

    Add files via upload

---
+++ Chat.js
@@ -0,0 +1,42 @@
+// src/components/Chat.js
+
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+
+const Chat = () => {
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
+  const socket = io('http://localhost:5000');
+
+  useEffect(() => {
+    socket.on('message', (message) => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    socket.emit('message', newMessage);
+    setNewMessage('');
+  };
+
+  return (
+    <div>
+      <div>
+        {messages.map((message, index) => (
+          <div key={index}>{message}</div>
+        ))}
+      </div>
+      <form onSubmit={sendMessage}>
+        <input
+          type="text"
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+        />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
+};
+
+export default Chat;
